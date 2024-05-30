47b3867ed1717c608f8466635e9b83102f14abca
Patricioherrera13 <patricioherrera133@gmail.com>
2024-05-30 14:12:20 -0300

    0.0.2

--- game.js
+++ game.js
@@ -1,23 +1,4 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var game = new Phaser.Game(config);
-
+// phaser/scenes/game-scene.js
 var player;
 var cursors;
 var level;
---
+++ main.js
@@ -0,0 +1,20 @@
+// phaser/phaser-config.js
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
